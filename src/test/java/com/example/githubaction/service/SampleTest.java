be4c4bb0eee58df68f8be2205375f99cde8b9635
unknown <dongmin.na@hotellotte.co.kr>
2022-07-20 13:39:40 +0900

    commit11

---
+++ src/test/java/com/example/githubaction/service/SampleTest.java
@@ -0,0 +1,21 @@
+package com.example.githubaction.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SampleTest {
+
+    @Value("${myname}")
+    String name;
+
+    @Test
+    public void test() {
+
+        Assertions.assertThat(name).isEqualTo("NaDongMin");
+//        Assertions.assertThat(name).isEqualTo("HongGilDong");
+
+    }
+}
